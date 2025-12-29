a7d6c883a5158dc583a8d8e8741d7bb93819a0d7
Hektor Wallin <hektor.wallin@eletive.com>
2025-12-29 13:35:43 +0100

    feat: update React and ReactDOM to canary versions and add ViewTransition to recipe route

--- app/src/routes/drinks/$drinkSlug/recipes/$recipeSlug.tsx
+++ app/src/routes/drinks/$drinkSlug/recipes/$recipeSlug.tsx
@@ -1,7 +1,7 @@
 import { RecipeDetails } from '@/components/RecipeDetails'
 import { RecipeList } from '@/components/RecipeList'
 import { createFileRoute } from '@tanstack/react-router'
-import { Suspense } from 'react'
+import { Suspense, ViewTransition } from 'react'
 
 export const Route = createFileRoute('/drinks/$drinkSlug/recipes/$recipeSlug')({
   component: RouteComponent,
@@ -11,13 +11,13 @@ function RouteComponent() {
   const { drinkSlug, recipeSlug } = Route.useParams()
 
   return (
-    <>
+    <ViewTransition>
       <Suspense fallback={<RecipeDetails.Skeleton />}>
         <RecipeDetails drinkSlug={drinkSlug} recipeSlug={recipeSlug} />
 
         <h2>Other Recipes</h2>
         <RecipeList drinkSlug={drinkSlug} />
       </Suspense>
-    </>
+    </ViewTransition>
   )
 }
